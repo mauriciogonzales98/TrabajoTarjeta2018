4f4dce54af492110bdb2113f787c8f17445eddf9
mauriciogonzales98 <39544631+mauriciogonzales98@users.noreply.github.com>
2018-08-11 12:44:24 -0300

    Add files via upload

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -10,7 +10,7 @@ class TarjetaTest extends TestCase {
      * Comprueba que la tarjeta aumenta su saldo cuando se carga saldo válido.
      */
     public function testCargaSaldo() {
-        $tarjeta = new Tarjeta;
+        $tarjeta = new Tarjeta();
 
         $this->assertTrue($tarjeta->recargar(10));
         $this->assertEquals($tarjeta->obtenerSaldo(), 10);
@@ -47,7 +47,7 @@ class TarjetaTest extends TestCase {
      * Comprueba que la tarjeta no puede cargar saldos invalidos.
      */
     public function testCargaSaldoInvalido() {
-      $tarjeta = new Tarjeta;
+      $tarjeta = new Tarjeta();
 
       $this->assertFalse($tarjeta->recargar(15));
       $this->assertEquals($tarjeta->obtenerSaldo(), 0);
