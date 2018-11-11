dda66905f77c2e5b08d128445c793c04d58ff535
mauriciogonzales98 <39544631+mauriciogonzales98@users.noreply.github.com>
2018-11-11 14:06:58 -0300

    Add files via upload

--- tests/TiempoTest.php
+++ tests/TiempoTest.php
@@ -9,14 +9,19 @@ class TiempoTest extends TestCase {
     public function testTiempoReal(){
         $tiempo = new Tiempo();
         $t=time();
+
         $this->assertEquals($tiempo->TiempoReal(),$t);
     }
 
     public function testTiempoFalso(){
         $tiempo= new Tiempo();
+        
+
         $this->assertEquals($tiempo->tiempoFalso(),0);
+        
         $tiempo->avanza(1);
-        $this->assertEquals($tiempo->tiempoFlaso(),1);
+        $this->assertEquals($tiempo->tiempoFalso(),1);
+        
         $tiempo->avanza(59);
         $this->assertEquals($tiempo->tiempoFalso(),60);
     }
