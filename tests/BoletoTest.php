4d9dcd371bebacdffd09b667801b22350da234d7
mauriciogonzales98 <39544631+mauriciogonzales98@users.noreply.github.com>
2018-11-11 11:15:17 -0300

    Tests arreglados.

--- tests/BoletoTest.php
+++ tests/BoletoTest.php
@@ -20,8 +20,10 @@ class BoletoTest extends TestCase {
         $boleto = new Boleto($valor, NULL, $tarjeta, NULL, 2);
 
         $this->assertEquals($boleto->obtenerSaldotarjeta(),0.0);
+        $tarjeta->recargar(20);
+        $this->assertEquals($boleto->obtenerSaldotarjeta(),20);
         $tarjeta->recargar(14.80);
-        $this->assertEquals($boleto->obtenerSaldotarjeta(),14.80);
+        $this->assertEquals($boleto->obtenerSaldotarjeta(),0.0);
     }
 
 
