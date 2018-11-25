39a432556ca56753ef1aeb23fbaefd0c5951e7b2
mauricio gonzales <mauri9825@gmail.com>
2018-11-25 01:05:33 -0300

    Ich will sterben 2

--- src/FranquiciaMedia.php
+++ src/FranquiciaMedia.php
@@ -3,7 +3,7 @@ namespace TrabajoTarjeta;
 
  class FranquiciaMedia Extends Tarjeta {    
 
-	function __construct($saldo=0.0, $precio=7.4, $viajePlus=2, $totaldeviajes=0, $Tipo=0, $ultimoboleto=0, $boleto=NULL){  
+	function __construct($saldo=0.0, $precio=7.4, $viajePlus=2, $totaldeviajes=0, $Tipo=2, $ultimoboleto=0, $boleto=NULL){  
 		$this->saldo = $saldo;
 		$this->precio = $precio;
 		$this->viajePlus = $viajePlus;
