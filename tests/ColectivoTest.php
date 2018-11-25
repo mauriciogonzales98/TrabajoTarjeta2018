5de7b0273c735880486790ee71f48cdb91a451a7
mauricio gonzales <mauri9825@gmail.com>
2018-11-25 15:02:59 -0300

    testeo

--- tests/ColectivoTest.php
+++ tests/ColectivoTest.php
@@ -198,7 +198,8 @@ class ColectivoTest extends TestCase {
 
     public function Trasbordo2(){
         $tarjeta = new Tarjeta($saldo = 100.0);
-        $tiempo = new TiempoFalso($segundos = 25200);
+        //$tiempo = new TiempoFalso($segundos = 25200);
+        $tiempo = new Tiempo();
         $colectivo = new Colectivo("112", "Mixta", "R");
         $colectivo2 = new Colectivo("110", "semtur", "N");
 
